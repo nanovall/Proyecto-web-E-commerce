b415147c692dbee32709301d80f890e126c82bfe
Lucas Roberto <LRoberto@farmesanet.com.ar>
2025-06-20 14:54:11 -0300

    se agrego el toast al modal

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -9,6 +9,7 @@ export function createModal(p) {
     const prod = productos.find((item) => item.id === id);
     if (prod) {
       guardarEnCarrito(prod);
+      mostrarToast("Producto agregado al carrito");
     }
   };
 
@@ -35,4 +36,30 @@ export function createModal(p) {
   `;
 
   containerModal.innerHTML = template;
+}
+
+export function mostrarToast(mensaje) {
+  const toastContainer = document.getElementById("toastContainer");
+
+  const toast = document.createElement("div");
+  toast.className = "toast align-items-center text-white bg-success border-0";
+  toast.role = "alert";
+  toast.ariaLive = "assertive";
+  toast.ariaAtomic = "true";
+  toast.innerHTML = `
+    <div class="d-flex">
+      <div class="toast-body">
+        ${mensaje}
+      </div>
+      <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+  `;
+
+  toastContainer.appendChild(toast);
+
+  const bsToast = new bootstrap.Toast(toast, { delay: 2000 });
+  bsToast.show();
+
+  // Eliminamos el toast del DOM cuando termina
+  toast.addEventListener('hidden.bs.toast', () => toast.remove());
 }
